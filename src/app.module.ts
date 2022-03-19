6adb0a2b9b8a35b8fcd8e24910543a035fc842d1
Murad <muradnden@gmail.com>
2022-03-19 19:11:18 +0200

    add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ZipsModule } from './zips/zips.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ 
+      isGlobal: true,
+    expandVariables:true }),
+    MongooseModule.forRoot(process.env.MONGODB_TRAINING_URI),
+    ZipsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT);
